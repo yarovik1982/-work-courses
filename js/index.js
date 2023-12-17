325dca8a5aeef30f2c8763342053bf607a8015e9
acunamatata82 <yar.oleg1982@mail.ru>
2023-12-17 15:22:37 +0300

    added counter width screen device

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 // import { removeClass } from "./modules/removeClass.js";
 
 import { useAccordeon } from "./modules/useAccordeon.js";
-
+console.log(window.innerWidth);
 useAccordeon()
+
+
+function main(elem) {
+   let widthDevise;
+ 
+   function updateWidth() {
+     const info = document.querySelector(elem);
+     info.innerHTML = "";
+ 
+     widthDevise = window.innerWidth;
+     info.innerHTML = widthDevise;
+   }
+ 
+   window.addEventListener("resize", updateWidth);
+ 
+   updateWidth();
+ }
+ main('.info')
